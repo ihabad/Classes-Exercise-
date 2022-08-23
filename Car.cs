cb1da53a4c342db154bb48a3f9ea56253f5991e7
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-08-22 19:42:15 -0700

    initial commit

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise1
+{
+    public class car
+
+    {
+        public car(string make, string model, int year)
+    {
+            Make = make;
+            Model = model;
+            Year = year;
+    }
+        public car()
+        {
+
+        }
+     
+        public string Make { get; set;}
+        public string Model { get; set;}
+        public int Year { get; set;}
+
+    }
+
+ }
+
+
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+namespace ClassesExercise1
+
+{
+   
+    internal class Program 
+
+    {
+        public static string Make { get; set; }
+        public static string Model { get; set; }
+        public static int Year { get; set; }
+
+
+        static void Main(string[] arg)
+        {
+         
+            car myCar = new car();
+
+            myCar.Make = "BMW";
+            myCar.Model = "i8";
+            myCar.Year = 2019;
+
+
+         
+
+            var audi = new car();
+            {
+                audi.Make = "audi";
+                audi.Model = "R8";
+                audi.Year = 2022;
+            }
+
+            var Honda = new car("Honda", "CV", 2018);
+
+            var carlist = new List<car>() { myCar, audi, Honda };
+
+
+
+            foreach (var item in carlist)
+            {
+
+
+                Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+                Console.WriteLine($"{audi.Make} {audi.Model} {audi.Year}");
+
+
+            }
+        }
+    }
+}
+
+
